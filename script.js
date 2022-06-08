d6cce543888e368e8cca5499c0b066c8bd4a96c3
TayJung <tay.jung29@gmail.com>
2022-06-08 20:48:46 -0300

    desafio completo

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = 'Taise'
+let numero = 20
+let anoAtual = 2022
+
+let subtracao = anoAtual - numero
+console.log(subtracao)
+
+let soma = subtracao + anoAtual
+console.log(soma)
+
+console.log('Estou mexendo no console.log')
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+
